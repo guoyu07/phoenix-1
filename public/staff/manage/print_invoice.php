e43d45638a14947b6bb414c45c9e473a4561d20c
yectep <yectep@gmail.com>
2013-07-15 15:14:17 +0800

    printable invoices

---
+++ public/staff/manage/print_invoice.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * View student data and schedule
+ *
+ * @author  Yectep Studios <info@yectep.hk>
+ * @version 30104
+ * @package Plume
+ */
+
+
+define('PTP',   '../../../private/');
+define('PHX_SCRIPT_TYPE',   'HTML');
+define('PHX_UX',        true);
+define('PHX_LAOSHI',    true);
+define('PHX_COURSES',   true);
+define('PHX_STUDENT',   true);
+
+// Include common ignition class
+require_once(PTP . 'php/ignition.php');
+
+// We require a staff login for this page
+if (!ACL::checkLogin('staff')) {
+    header('Location: /staff/index.php?msg=error_nologin');
+    exit();
+} else {
+    $_laoshi = new Laoshi($_SESSION['SSOID']);
+    $_laoshi->perms(8,9,10,11,12);
+}
+
+// Get course information
+$fam = FamStu::getFamilyById($_REQUEST['fid']);
+$p['children'] = '';
+$p['family_name'] = $fam['family']['FamilyName'];
+$p['family_id'] = $fam['family']['FamilyID'];
+$p['family_email'] = $fam['family']['FamilyEmail'];
+$p['family_hphone'] = ((strlen($fam['family']['FamilyPhoneHome']) == 8) ? '<span class="muted">+852</span> '.substr($fam['family']['FamilyPhoneHome'], 0, 4).'-'.substr($fam['family']['FamilyPhoneHome'],4,4) : $fam['family']['FamilyPhoneHome']);
+$p['family_mphone'] = ((strlen($fam['family']['FamilyPhoneMobile']) == 8) ? '<span class="muted">+852</span> '.substr($fam['family']['FamilyPhoneMobile'], 0, 4).'-'.substr($fam['family']['FamilyPhoneMobile'],4,4) : $fam['family']['FamilyPhoneMobile']);
+$p['family_cts'] = date(DATETIME_FULL, strtotime($fam['family']['FamilyCTS']));
+$p['family_address'] = nl2br($fam['family']['FamilyAddress']);
+$p['family_llts'] = date(DATETIME_FULL, strtotime($fam['family']['FamilyLLTS'])+28800);
+$p['family_lats'] = date(DATETIME_FULL, strtotime($fam['family']['FamilyLATS'])+28800);
+$p['family_comments'] = $fam['family']['FamilyIC'];
+
+// From invoices
+$today = new DateTime();
+
+$stmt = Data::prepare('SELECT e.*, cl.ClassWeek, s.StudentNameGiven, s.StudentNamePreferred, s.StudentNameLast, cl.ClassPeriodBegin, cl.ClassPeriodEnd, co.CourseSubj, co.CourseID, co.CourseTitle FROM enrollment e, classes cl, courses co, students s, families f WHERE f.FamilyID = :fid AND f.FamilyID = s.FamilyID AND s.StudentID = e.StudentID AND cl.ClassID = e.ClassID AND cl.CourseID = co.CourseID AND e.EnrollStatus = "enrolled" ORDER BY cl.ClassWeek ASC, cl.ClassPeriodBegin ASC');
+$stmt->bindParam('fid', $_REQUEST['fid']);
+$stmt->execute();
+
+$invoice = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = Data::prepare('SELECT * FROM payments WHERE FamilyID = :fid');
+$stmt->bindParam('fid', $_REQUEST['fid']);
+$stmt->execute();
+
+$payments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$p['billed_lines'] = '';
+$total = 0;
+
+foreach($invoice as $i => $line) {
+    if (($line['CourseSubj'] == 'PHED') || ($line['CourseSubj'] == 'LANG') || ($line['CourseSubj'] == 'MSCT') || ($line['CourseSubj'] == 'ARTS')) {
+        $amount = (($line['ClassPeriodBegin'] == $line['ClassPeriodEnd']) ? '800' : '1600');
+    } else {
+        $amount = '2500';
+    }
+
+    if ($line['ClassWeek'] == 2) $amount = floor($amount*0.8);
+
+    $p['billed_line'] .= '<tr><td>'.date(DATE_SHORT, strtotime($line['EnrollCTS'])).'</td><td>'.strtoupper($line['CourseSubj']).str_pad($line['CourseID'], 3, '0', STR_PAD_LEFT).' - W'.$line['ClassWeek'].'P'.$line['ClassPeriodBegin'].' <em class="muted small">'.$line['StudentNamePreferred'].' '.$line['StudentNameLast'].'</td><td>'.Courses::getDueDate($line['ClassWeek']).'</td><td style="text-align:right;">$'.number_format($amount).'</td></tr>';
+    $total += $amount;
+}
+
+$p['total_due'] = number_format($total);
+
+foreach($payments as $i => $line) {
+    $p['charge_lines'] .= '<tr class="'.(($line['PayVerified'] == 1) ? "success" : "pending").'""><td>'.$line['PayMethod'].'</span></td><td>'.date(DATE_SHORT, strtotime($line['PayCTS'])).'</td><td>'.(($line['PayVerified'] == 0) ? '<a href="/staff/manage/verify_invoice.php?pid='.$line['PayID'].'" class="tipped" title="Click to verify this reported payment">'.$line['PayDesc'].'</a>' : $line['PayDesc']).'</td><td style="text-align:right;">'.(($line['PayVerified'] == 1) ? '$'.number_format($line['PayAmount']) : '<em class="muted">Pending</em>').'</td></tr>';
+    (($line['PayVerified'] == 1) ? $charges += $line['PayAmount'] : $charges = $charges);
+}
+
+$p['total_paid'] = number_format($charges);
+$p['family_id'] = $_REQUEST['fid'];
+
+$final_invoice = - $charges - $total;
+$p['final_class'] = (($final_invoice < 0) ? "error" : (($final_invoice == 0) ? "success" : "pending"));
+$p['final_invoice'] = number_format((-1)*($final_invoice));
+
+
+echo UX::grabPage('staff/manage/print_invoice', $p, true);
+
+?>
+
